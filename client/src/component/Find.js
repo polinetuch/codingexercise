0beb2be6087b98c2d482313fd6ea7ffa7de2b9bf
Polinet <uchpolinet@gmail.com>
2020-05-20 16:49:26 +1000

    added handleSubmit function

--- client/src/component/Find.js
+++ client/src/component/Find.js
@@ -8,6 +8,7 @@ class Find extends React.Component {
             url: ''}
 
         this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
@@ -15,14 +16,27 @@ class Find extends React.Component {
             text: event.target.value,
             url: event.target.value
         })
-    } 
+    } ;
+
+    handleSubmit(event) {
+        console.log("Text: " + this.state.value.text);
+        console.log("URL: " + this.state.value.url);
+        event.preventDefault();
+    }
     render() {
         return(
+            <>
+            <h3>Search here to find out</h3>
             <form>
-                <h3>Search here to find out</h3>
                 <p>Enter any word here:</p>
-                <input type="text" onChange={this.handleChange}/>
+                <input type="text" id="text" onChange={this.handleChange}/>
             </form>
+            <form>
+                <p>Enter any url here:</p>
+                <input type="url" id="url" onChange={this.handleChange}/>
+            </form>
+            <button id="submit-btn" onSubmit={this.handleSubmit}>Find</button>
+            </>
         )
     }
 
