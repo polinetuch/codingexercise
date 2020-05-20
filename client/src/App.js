d876f6737bea3f26722e17d822be3fb47f874479
Polinet <uchpolinet@gmail.com>
2020-05-20 16:43:03 +1000

    import Find component to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Find from './component/Find';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        Find out how popular you are!
       </header>
+      <div>
+        <Find>
+          <label>
+            Enter any word here: 
+            <input type="text"></input>
+          </label>
+        </Find>
+      </div>
     </div>
   );
 }
