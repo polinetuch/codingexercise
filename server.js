cc2b0e63def8d564375cc248f5109bbb8b8feeee
Polinet <uchpolinet@gmail.com>
2020-05-20 16:24:34 +1000

    require dependencies & set port to 8080

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-const express = require('express');
+// Require all dependencies to set up server
+var express = require('express');
+var logger = require('morgan');
+var mongoose = require('mongoose');
+
+var axios = require('axios');
+var cheerio = require('cheerio');
+
+// Set port number to 8080
+var PORT = 8080;
+
+// Initialize express
+var app = express();
